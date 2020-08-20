703977e6380fc84c905387bd4be01ea1c528578c
Changyoon-Lee <lkingkongl@naver.com>
2020-08-20 19:55:57 +0900

    EDA update

---
+++ 2020빅콘테스트 문제데이터(혁신아이디어분야)/03_SNS데이터(와이즈넛)/새 텍스트 문서.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+df = pd.read_csv('Downloads/2020빅콘테스트 문제데이터(혁신아이디어분야)/03_SNS데이터(와이즈넛)/2020 bigcontest data_wisenut.csv', delimiter='')
+print(df.head(20))
+#\Downloads\2020빅콘테스트 문제데이터(혁신아이디어분야)\03_SNS데이터(와이즈넛)
---
+++ 2020빅콘테스트 문제데이터(혁신아이디어분야)/test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('03_SNS데이터(와이즈넛)/2020 bigcontest data_wisenut.csv', delimiter='')
+df.head()
